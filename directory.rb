0659f4080b02c2a68aad51cf5ceb75c6177c436d
Kesha Kumshayev <kumshkesh@gmail.com>
2017-11-10 16:13:09 +0000

    switched back to the .each method for printing students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
 #first we put the list of students in an array
-students = [
-	{name: "Dr. Hannibal Lecter", cohort: :november},
-	{name: "Darth Vader", cohort: :november},
-	{name: "Nurse Ratched", cohort: :november},
-	{name: "Michael Corleone", cohort: :november},
-	{name: "Alex DeLarge", cohort: :november},
-	{name: "The Wicked Witch of the West", cohort: :november},
-	{name: "Terminator", cohort: :november},
-	{name: "Freddy Krueger", cohort: :november},
-	{name: "The Joker", cohort: :november},
-	{name: "Joffrey Baratheon", cohort: :november},
-	{name: "Norman Bates", cohort: :november}
-]
+# students = [
+# 	{name: "Dr. Hannibal Lecter", cohort: :november},
+# 	{name: "Darth Vader", cohort: :november},
+# 	{name: "Nurse Ratched", cohort: :november},
+# 	{name: "Michael Corleone", cohort: :november},
+# 	{name: "Alex DeLarge", cohort: :november},
+# 	{name: "The Wicked Witch of the West", cohort: :november},
+# 	{name: "Terminator", cohort: :november},
+# 	{name: "Freddy Krueger", cohort: :november},
+# 	{name: "The Joker", cohort: :november},
+# 	{name: "Joffrey Baratheon", cohort: :november},
+# 	{name: "Norman Bates", cohort: :november}
+# ]
+## ABOVE IS THE ORIGINAL ARRAY NOW WE INPUT STUDENTS
+## WHEN WE RUN THE PROGRAM
 
 def print_header
 	puts "The students of Villains Academy"
@@ -19,10 +21,12 @@ def print_header
 end
 
 def print(names)
-	name_count = 0
-	while names[name_count] != nil # While there is an entry for the student in our array, print their info.
-		puts "#{names.index(names[name_count])+1}: #{names[name_count][:name]} (#{names[name_count][:cohort]} cohort)" 
-		name_count += 1
+	names.each do |student| # back to the .each method
+		puts "#{names.index(student)+1}: #{student[:name]} |"+
+			" #{student[:cohort]} cohort |"+
+			"Hobbies include: #{student[:hobbies]} |"+
+			"Born in: #{student[:country_of_birth]} |"+
+			"#{student[:height]} tall." 
 	end
 end
 
@@ -38,7 +42,7 @@ def input_students
 	name = gets.chomp
 	# while the name is not empty, repeat the following:
 	while !name.empty? do
-		students << {name: name, cohort: :november}
+		students << {name: name, cohort: :november, hobbies: :skydiving, country_of_birth: :UK, height: :'189cm'}
 		puts "Now we have #{students.count} students"
 		#get amother name from the user
 		name = gets.chomp
